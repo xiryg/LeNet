99c7a3a7c290d93a4ea72bd87959eabe5ba41f8e
xiryg <xry102410@gmail.com>
2024-10-27 19:06:36 +0800

    数据加载~ MNIST数据集 （添加数据划分）

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 
-def load_data(batch_size):
+def load_data(batch_size, val_split=0.2):
     transform = transforms.Compose(
         [
             transforms.Resize((32, 32)),
@@ -14,7 +14,15 @@ def load_data(batch_size):
     train_set = datasets.MNIST(root='./data', train=True, transform=transform, download=True)
     test_set = datasets.MNIST(root='./data', train=False, transform=transform, download=True)
 
+    # 划分训练集和验证集
+    val_size = int(val_split * len(train_set))
+    train_size = len(train_set) - val_size
+    train_set, val_set = random_split(train_set, [train_size, val_size])
+
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
     test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader
 
     return train_loader, test_loader
