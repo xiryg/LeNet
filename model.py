783da5d41871d8f47e07bfacb612a830701282ed
xiryg <xry102410@gmail.com>
2024-10-27 18:09:51 +0800

    initialize~

---
+++ model.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 6, 5, padding=2),
+            nn.Sigmoid(),
+            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(6, 16, 5),
+            nn.Sigmoid(),
+            nn.AvgPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(16 * 5 * 5, 120),
+            nn.Sigmoid(),
+            nn.Linear(120, 84),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+
+model = LeNet()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+summary(model, (1, 28, 28))
